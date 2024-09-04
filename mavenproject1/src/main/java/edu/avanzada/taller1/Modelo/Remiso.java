77bb0ba3dda009746244463c853ea7dec816b22d
Julian lopez <juguilopez@gmail.com>
2024-09-04 14:16:44 -0500

    CompletitudDeClases

--- mavenproject1/src/main/java/edu/avanzada/taller1/Modelo/Remiso.java
+++ mavenproject1/src/main/java/edu/avanzada/taller1/Modelo/Remiso.java
@@ -4,21 +4,11 @@
  */
 package edu.avanzada.taller1.Modelo;
 
-/**
- *
- * @author JULIAN
- */
-<<<<<<< HEAD:mavenproject1/src/main/java/edu/avanzada/taller1/Modelo/Remiso.java
-public class Remiso extends Persona{
+public class Remiso extends Persona {
 
-    public Remiso(String nombre, String cedula, String apellido) {
-        super(nombre, cedula, apellido,  "Remiso");
+    public Remiso(String cedula, String nombre, String apellido) {
+        super(cedula, nombre, apellido, "Remiso");
     }
-    
-=======
-public class ControlPersona {
-    private Persona persona;
-    private Vista ventana;
-  
->>>>>>> 63913295d43d0fd4c8505fd7c71abb08d2a12453:mavenproject1/src/main/java/Controlador/ControlPersona.java
+
+    // Métodos específicos de Remiso...
 }
